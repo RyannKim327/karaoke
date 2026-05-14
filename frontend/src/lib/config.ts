b63b34cde01bc3eb3f81da1fb25e40c657101eb7
RyannKim327 <rksesgundo123@gmail.com>
2026-05-14 15:23:50 +0800

    05-14-26 15:23

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,3 +1,4 @@
 const HOST = "karakokey.leapcell.app"
+const WS = "karaoke-ls7t.onrender.com"
 export const API_HOST = `https://${HOST}`;
-export const WS_HOST = `wss://${HOST}`;
+export const WS_HOST = `wss://${WS}`;
